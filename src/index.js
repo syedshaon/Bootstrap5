b0e9d68a55c87e181fb0b8cca3f8453fdb6ec831
syedshaon <syedshaon99@gmail.com>
2023-05-03 21:42:47 +0600

    not working

--- src/index.js
+++ src/index.js
@@ -2,8 +2,18 @@
 // Will be using reset css through scss
 
 //  TO DISABLE DONT IMPORT * AS bOOTSTRAP ALSO DON'T IMPORT BOOTSTRAP IN STYLE.SCSS FILE
+/* import "bootstrap"; */
+
 import "./style.scss";
 // Import all of Bootstrap's JS
-import * as bootstrap from "bootstrap";
+/* import * as bootstrap from "bootstrap"; */
+
+// import { Util, Dropdown, Offcanvas, Popover } from "bootstrap";
 // or, specify which plugins you need:
 // import { Tooltip, Toast, Popover } from 'bootstrap'
+
+/* Import Images */
+
+import "./assets/fundamentals.svg";
+import "./assets/react.svg";
+import "./assets/showcase.svg";
